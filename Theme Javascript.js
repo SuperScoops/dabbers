ff3d5f18fe72bc646ee5ca8ff9a3f79b73b4390d
Mickey Malone <SuperScoops@users.noreply.github.com>
2019-07-02 02:36:16 +1000

    Add files via upload

---
+++ Theme Javascript.js
@@ -0,0 +1,45 @@
+﻿/*!
+**|   XaeMae Sequenced Module Loader
+**|   DO NOT REUPLOAD
+**@preserve
+*/
+// -- Channel Namespace --
+if (!this[CHANNEL.name])
+    this[CHANNEL.name] = {};
+// -- The Module Library
+window[CHANNEL.name].sequenceList = {
+	'layout':      { active: 1, rank: -1, url: "//rawgit.com/SuperScoops/dabbers/channelbase.js",              callback: true },
+    'channel':      { active: 1, rank: -1, url: "//rawgit.com/SuperScoops/dabbers/enhance.js",              callback: true },
+};
+
+window[CHANNEL.name].sequencePrev = window[CHANNEL.name].sequencePrev || "";
+window[CHANNEL.name].sequenceState = window[CHANNEL.name].sequenceState || 0;
+window[CHANNEL.name].sequenceIndex = Object.keys(window[CHANNEL.name].sequenceList)
+
+window[CHANNEL.name].sequencerLoader = function (){
+    // After first run we curry the previous modules callback
+    // This is mainly used to reassign variables in modules/scripts that don't use module options
+    if(window[CHANNEL.name].sequencePrev){
+        setTimeout(window[CHANNEL.name].sequenceList[window[CHANNEL.name].sequencePrev].callback, 0)
+        window[CHANNEL.name].sequencePrev = "";
+    }
+
+    if(window[CHANNEL.name].sequenceState >= window[CHANNEL.name].sequenceIndex.length){
+        return (function(){ console.log("Xaekai's Script Sequencer: Loading Complete.") })()
+    }
+
+    var currKey = window[CHANNEL.name].sequenceIndex[window[CHANNEL.name].sequenceState];
+    if(window[CHANNEL.name].sequenceState < window[CHANNEL.name].sequenceIndex.length){
+        if(window[CHANNEL.name].sequenceList[currKey].active
+            && window[CHANNEL.name].sequenceList[currKey].rank <= CLIENT.rank
+        ){
+            console.log("Xaekai's Script Sequencer: Loading " + currKey);
+            window[CHANNEL.name].sequencePrev = currKey;
+            window[CHANNEL.name].sequenceState++;
+            $.getScript(window[CHANNEL.name].sequenceList[currKey].url, window[CHANNEL.name].sequencerLoader)
+        } else {
+            window[CHANNEL.name].sequenceState++;
+            window[CHANNEL.name].sequencerLoader()
+        }
+    }
+};window[CHANNEL.name].sequencerLoader()
