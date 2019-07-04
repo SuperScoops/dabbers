dbd8ea2dd6f5276e0fdfe9b9a80e33d00b0d53b6
Mickey Malone <SuperScoops@users.noreply.github.com>
2019-07-04 14:37:21 +1000

    Add files via upload

--- CustomEdit15.js
+++ CustomEdit15.js
@@ -686,7 +686,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-53755606-1', 'auto');
 ga('send', 'pageview');
 
-var bgColorArray = [''],
+var bgColorArray = ['//i.ibb.co/GTfSQzd/image.png'],
 
 selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 $('#backg').css('background', 'url(' + selectBG + ')')
